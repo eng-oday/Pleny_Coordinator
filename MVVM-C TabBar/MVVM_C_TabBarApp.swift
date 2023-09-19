72285d17d8b5689a570a2fa732e358aa053ea79a
eng-oday <odaydieg@gmail.com>
2023-09-19 13:56:30 +0300

    Initial Commit

---
+++ MVVM-C TabBar/MVVM_C_TabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_C_TabBarApp.swift
+//  MVVM-C TabBar
+//
+//  Created by 3rabApp-oday on 19/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_C_TabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
